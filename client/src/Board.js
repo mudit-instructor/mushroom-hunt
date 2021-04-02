1c9b42de479250f32903b2cbbaebdd119b218530
Mudit Goel <mudit.goel@ivanti.com>
2021-04-02 19:41:32 +0530

    randomly added mushrooms to the board

--- client/src/Board.js
+++ client/src/Board.js
@@ -1,7 +1,8 @@
 import Row from "./Row.js";
 import Cell from "./Cell.js";
 import React from "react";
-import './Board.css';
+import "./Board.css";
+import _ from "lodash";
 
 function repeatCell(noOfCols) {
   var cellArr = [];
@@ -12,46 +13,59 @@ function repeatCell(noOfCols) {
 }
 
 function repeatRows(noOfRows, noOfCols) {
-    var rowArr = [];
-    for (var i = 0; i < noOfRows; i++) {
-        rowArr.push(<Row  key={i}>{repeatCell(noOfCols)}</Row>);
-      }
-      return rowArr;
+  var rowArr = [];
+  for (var i = 0; i < noOfRows; i++) {
+    rowArr.push(<Row key={i}>{repeatCell(noOfCols)}</Row>);
+  }
+  return rowArr;
 }
 
 function createGrid(props) {
-
-  var grid = [], row = [];
+  var grid = [],
+    row = [];
 
   for (var ro = 0; ro < props.rows; ro++) {
-      row = [];
+    row = [];
     for (var col = 0; col < props.cols; col++) {
-      row.push(ro+"-"+col);
+      row.push(ro + "-" + col);
     }
     grid.push(row);
   }
-   return grid;
+  return grid;
 }
 
 function Board(props) {
-
-  var plr_row_mid = Math.floor(props.rows/2);
-  var plr_col_mid = Math.floor(props.cols/2);
+  var plr_row_mid = Math.floor(props.rows / 2);
+  var plr_col_mid = Math.floor(props.cols / 2);
 
   var grid = createGrid(props);
 
-  var gridJSX = grid.map(function(row, indx) {
-    var rowJSX = row.map(function(cell, cid) {
-      return <Cell key={cell} id={cell} plr_row_mid={plr_row_mid} plr_col_mid={plr_col_mid}/>;
+  var flatGrid = _.flatten(grid);
+
+  var flatGridWithoutPlr = flatGrid.filter(
+    (item) => item !== plr_row_mid + "-" + plr_col_mid
+  );
+
+  var randomChosenPos = _.sampleSize(flatGridWithoutPlr, props.cols);
+
+  var gridJSX = grid.map(function (row, indx) {
+    var rowJSX = row.map(function (cell, cid) {
+      return (
+        <Cell
+          key={cell}
+          id={cell}
+          plr_row_mid={plr_row_mid}
+          plr_col_mid={plr_col_mid}
+          randomChosenPos={randomChosenPos}
+        />
+      );
     });
-    return <Row key={indx}>{rowJSX}</Row>
+    return <Row key={indx}>{rowJSX}</Row>;
   });
 
   return (
-      <div className="parentOfBoard">
-    <div className="board">
-      {gridJSX}
-    </div>
+    <div className="parentOfBoard">
+      <div className="board">{gridJSX}</div>
     </div>
   );
 }
--- client/src/Cell.js
+++ client/src/Cell.js
@@ -1,23 +1,31 @@
-import './Cell.css';
+import "./Cell.css";
 
 function isPlrThrinCell(props) {
+  var cellId = props.id;
+  var plrCellId = props.plr_row_mid + "-" + props.plr_col_mid;
 
-    var cellId = props.id;
-    var plrCellId = props.plr_row_mid+"-"+props.plr_col_mid;
+  var isPlrThrinCell = cellId === plrCellId;
+  return isPlrThrinCell;
+}
 
-    var isPlrThrinCell = cellId === plrCellId;
-    return isPlrThrinCell;
+function isMushroomHome(props) {
+  return props.randomChosenPos.indexOf(props.id) >= 0;
 }
 
 function Cell(props) {
-    var baseCls = "cell";
+  var baseCls = "cell";
 
-    if (isPlrThrinCell(props)) {
-        var topUpCls = "hasPlayer";
-        baseCls = baseCls + " " + topUpCls;
-    }
-    
-    return <div id = {props.id} className={baseCls}></div>
+  if (isPlrThrinCell(props)) {
+    var topUpCls = "hasPlayer";
+    baseCls = baseCls + " " + topUpCls;
+  }
+
+  if (isMushroomHome(props)) {
+    var topUpCls = "hasMushroom";
+    baseCls = baseCls + " " + topUpCls;
+  }
+
+  return <div id={props.id} className={baseCls}></div>;
 }
 
 export default Cell;
